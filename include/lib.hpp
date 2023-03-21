befd8db488b78522253e8ef297665299c444827e
Sadhbh Code <sonia.kolasinska.pro@gmail.com>
2023-03-21 21:19:48 +0000

    First iteration - simple implementation w/ tests

--- include/lib.hpp
+++ include/lib.hpp
@@ -1,7 +1,168 @@
 #ifndef INCLUDED_LIB_HPP
 #define INCLUDED_LIB_HPP
 
-namespace sadhbhcraft::orderbook {
+#include<string>
+#include<deque>
+#include<algorithm>
 
-}// end of namespace sadhbhcraft::orderbook
-#endif//INCLUDED_LIB_HPP
+namespace sadhbhcraft::orderbook
+{
+    enum class Side
+    {
+        Buy,
+        Sell
+    };
+
+    enum class OrderType
+    {
+        Market,
+        Limit,
+        IOC,
+        FOC
+    };
+
+    struct Instrument
+    {
+        std::string symbol;
+    };
+
+    struct Market
+    {
+        Instrument &main;
+        Instrument &traded;
+    };
+
+    struct Order
+    {
+        Market &market;
+        Side side;
+        OrderType order_type;
+        int price;
+        int quantity;
+    };
+
+    struct OrderQuantity
+    {
+        OrderQuantity(Order &order): order(order)
+        {}
+
+        Order &order;
+        int quantity;
+    };
+
+    class OrderPriceLevel
+    {
+    public:
+        OrderPriceLevel(int price): m_price(price)
+        {}
+
+        void add_order(Order &order)
+        {
+            m_orders.push_back(order);
+        }
+
+        const int price() const { return m_price; }
+
+        std::deque<OrderQuantity>::const_iterator begin() const { return m_orders.begin(); }
+        std::deque<OrderQuantity>::const_iterator end() const { return m_orders.end(); }
+
+        OrderQuantity &first() { return m_orders.front(); }
+        const OrderQuantity &first() const { return m_orders.front(); }
+
+        size_t size() const { return m_orders.size(); }
+        bool empty() const { return m_orders.empty(); }
+
+    private:
+        std::deque<OrderQuantity> m_orders;
+        int m_price;
+    };
+
+    struct OrderPriceLevelCompare
+    {
+        OrderPriceLevelCompare(Side side): m_side(side){}
+
+        bool operator()(const OrderPriceLevel &a, const OrderPriceLevel &b) const
+        {
+            return (m_side == Side::Buy) ? b.price() < a.price() : a.price() < b.price();
+        }
+
+        bool operator()(int price, const OrderPriceLevel &b) const
+        {
+            return (m_side == Side::Buy) ? b.price() < price : price < b.price();
+        }
+
+        bool operator()(const OrderPriceLevel &a, int price) const
+        {
+            return (m_side == Side::Buy) ? price < a.price() : a.price() < price;
+        }
+
+    private:
+        Side m_side;
+    };
+
+
+    class OrderBookSide
+    {
+    public:
+        OrderBookSide(Side side): m_side(side)
+        {}
+
+        void add_order(Order &order)
+        {
+            std::deque<OrderPriceLevel>::iterator level_iterator = find_or_get_insert_iterator(order.price);
+            if (level_iterator == m_levels.end() || level_iterator->price() != order.price)
+            {
+                level_iterator = m_levels.insert(level_iterator, OrderPriceLevel(order.price));
+            }
+            level_iterator->add_order(order);
+        }
+
+        Side side() const { return m_side ;}
+
+        std::deque<OrderPriceLevel>::const_iterator begin() const { return m_levels.begin(); }
+        std::deque<OrderPriceLevel>::const_iterator end() const { return m_levels.end(); }
+
+        OrderPriceLevel &top() { return m_levels.front(); }
+        const OrderPriceLevel &top() const { return m_levels.front(); }
+
+        size_t size() const { return m_levels.size(); }
+        bool empty() const { return m_levels.empty(); }
+
+    private:
+        std::deque<OrderPriceLevel> m_levels;
+        Side m_side;
+
+        std::deque<OrderPriceLevel>::iterator find_or_get_insert_iterator(int price)
+        {
+            return std::lower_bound(m_levels.begin(), m_levels.end(), price, OrderPriceLevelCompare(m_side));
+        }
+    };
+
+    class OrderBook
+    {
+    public:
+        OrderBook(): m_bid(Side::Buy), m_ask(Side::Sell)
+        {}
+
+        void accept_order(Order &order)
+        {
+            if (order.side == Side::Buy)
+            {
+                m_bid.add_order(order);
+            }
+            else
+            {
+                m_ask.add_order(order);
+            }
+        }
+
+        const OrderBookSide &bid() const { return m_bid; }
+        const OrderBookSide &ask() const { return m_ask; }
+
+    private:
+        OrderBookSide m_bid;
+        OrderBookSide m_ask;
+    };
+
+} // end of namespace sadhbhcraft::orderbook
+#endif // INCLUDED_LIB_HPP
--- tests/test_lib.cpp
+++ tests/test_lib.cpp
@@ -8,8 +8,170 @@ int main(int argc, const char** argv)
 {
     namespace scob = sadhbhcraft::orderbook;
 
+    scob::Instrument i1{.symbol = "AAA"};
+    scob::Instrument i2{.symbol = "ABB"};
 
-    assert(true);
+    scob::Market m1{.main=i1, .traded=i2};
+    scob::OrderBook book;
+
+    // 1. When book is empty we expect an order to be added to correct side at
+    // new level
+    // Add first order
+    scob::Order o1{
+        .market = m1,
+        .side = scob::Side::Buy,
+        .order_type = scob::OrderType::Limit,
+        .price = 100,
+        .quantity = 5
+    };
+
+    book.accept_order(o1);
+
+    // Expect: 1-level, 1-element queue
+    assert(book.bid().size() == 1);
+    assert(book.bid().top().size() == 1);
+    assert(&(book.bid().top().first().order) == &o1);
+
+    // 2. When book contains one level with one order, new order at same price
+    // will join at the end of the queue
+    // Add second order, same price
+    scob::Order o2{
+        .market = m1,
+        .side = scob::Side::Buy,
+        .order_type = scob::OrderType::Limit,
+        .price = 100,
+        .quantity = 10
+    };
+
+    book.accept_order(o2);
+
+    // Expect: 1-level, 2-element queue
+    assert(book.bid().size() == 1);
+    assert(book.bid().top().size() == 2);
+    assert(&(book.bid().top().first().order) == &o1);
+    assert(&(std::next(book.bid().top().begin())->order) == &o2);
+
+    // 3. When book contains one level, new order at worse price will be added
+    // to a new deeper level
+    // Add third order, lower price
+    scob::Order o3{
+        .market = m1,
+        .side = scob::Side::Buy,
+        .order_type = scob::OrderType::Limit,
+        .price = 90,
+        .quantity = 5
+    };
+
+    book.accept_order(o3);
+
+    // Expect: 2-levels
+    assert(book.bid().size() == 2);
+    // Expect: 2-el in 1st level queue
+    assert(book.bid().top().size() == 2);
+    assert(&(book.bid().top().first().order) == &o1);
+    assert(&(std::next(book.bid().top().begin())->order) == &o2);
+    // Expect: 1-el in 2nd level queue
+    assert(std::next(book.bid().begin())->size() == 1);
+    assert(&(std::next(book.bid().begin())->first().order) == &o3);
+
+    // 4. When book contains two levels, new order with price in between the two
+    // levels will be added to a new level in between
+    // Add fourth order, middle price
+    scob::Order o4{
+        .market = m1,
+        .side = scob::Side::Buy,
+        .order_type = scob::OrderType::Limit,
+        .price = 95,
+        .quantity = 10
+    };
+
+    book.accept_order(o4);
+
+    // Expect: 3-levels
+    assert(book.bid().size() == 3);
+    // Expect: 2-el in 1st level queue
+    assert(book.bid().top().size() == 2);
+    assert(&(book.bid().top().first().order) == &o1);
+    assert(&(std::next(book.bid().top().begin())->order) == &o2);
+    // Expect: 1-el in 2nd level queue
+    assert(std::next(book.bid().begin())->size() == 1);
+    assert(&(std::next(book.bid().begin())->first().order) == &o4);
+    // Expect: 1-el in 3nd level queue
+    assert(std::next(book.bid().begin(), 2)->size() == 1);
+    assert(&(std::next(book.bid().begin(), 2)->first().order) == &o3);
+    
+    // 5. When book contains any levels, new order with new best price will be
+    // added to new level at the top of the book
+    // Add 5th order, best bid
+    scob::Order o5{
+        .market = m1,
+        .side = scob::Side::Buy,
+        .order_type = scob::OrderType::Limit,
+        .price = 105,
+        .quantity = 2
+    };
+
+    // 6. When book contains bids, and no asks, then new sell order will be
+    // added to ask side, and bid side won't be altered
+    // Add 6th order, best ask
+    scob::Order o6{
+        .market = m1,
+        .side = scob::Side::Sell,
+        .order_type = scob::OrderType::Limit,
+        .price = 120,
+        .quantity = 7
+    };
+
+    book.accept_order(o5);
+    book.accept_order(o6);
+    
+    // Expect: 1-level at Ask
+    assert(book.ask().size() == 1);
+    // Expect: 1-el in 1st level queue
+    assert(book.ask().top().size() == 1);
+    assert(&(book.ask().top().first().order) == &o6);
+
+    // Expect: 4-levels at Bid
+    assert(book.bid().size() == 4);
+    // Expect: 1-el in 1st level queue
+    assert(book.bid().top().size() == 1);
+    assert(&(book.bid().top().first().order) == &o5);
+    // Expect: 2-el in 2nd level queue
+    assert(std::next(book.bid().begin())->size() == 2);
+    assert(&(std::next(book.bid().begin())->first().order) == &o1);
+    assert(&(std::next(std::next(book.bid().begin())->begin())->order) == &o2);
+    // Expect: 1-el in 3rd level queue
+    assert(std::next(book.bid().begin(), 2)->size() == 1);
+    assert(&(std::next(book.bid().begin(), 2)->first().order) == &o4);
+    // Expect: 1-el in 4th level queue
+    assert(std::next(book.bid().begin(), 3)->size() == 1);
+    assert(&(std::next(book.bid().begin(), 3)->first().order) == &o3);
+    
+    // 7. When book contains bids and asks, new Sell order with worse price will
+    // be added to new deeper level on the ask side, and number of bid levels won't be altered
+    // NOTE: We have already checked for bids that insertion at different price
+    // points works correctly, and the purpose of this test is to confirm that
+    // ask side has opposite sorting direction than bid side
+    // Add 7th order, 2nd level ask
+    scob::Order o7{
+        .market = m1,
+        .side = scob::Side::Sell,
+        .order_type = scob::OrderType::Limit,
+        .price = 125,
+        .quantity = 4
+    };
+
+    book.accept_order(o7);
+
+    // Expect: 2-levels at Ask
+    assert(book.ask().size() == 2);
+    // Expect: 1-el in 1st level queue
+    assert(book.ask().top().size() == 1);
+    assert(&(book.ask().top().first().order) == &o6);
+    // Expect: 1-el in 2nd level queue
+    assert(std::next(book.ask().begin())->size() == 1);
+    assert(&(std::next(book.ask().begin())->first().order) == &o7);
+
 
     std::cout << "Tests OK." << std::endl;
     return 0;
